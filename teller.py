5faa3801bdabbb1c30281a7bb46f6fc4af49299c
Sean Baskin <sean.baskin@bnl-consulting.com>
2019-02-02 20:25:39 -0500

    Added additional comments

--- teller.py
+++ teller.py
@@ -7,13 +7,16 @@ def clean(input, output):
 
     TODO: Explain the format of the original files.
     """
+
+    # Read the input file into a list without the pre-header information
     with open(input) as f:
         transactions = f.readlines()[7:]
 
+    # Write the cleaned data to a new file
     with open(output, 'w') as f:
         for record in transactions:
             f.write(record)
 
 
-# Run the function
+# Run the function using the second and third arguments
 clean(sys.argv[1], sys.argv[2])
